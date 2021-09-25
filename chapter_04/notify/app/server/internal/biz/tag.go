37f52b333feb89c397c15d3f554ad84cc1e6f12b
张斌 <zhangbin@maizijf.com>
2021-09-26 00:42:04 +0800

    调整目录结构

--- chapter_04/notify/app/server/internal/biz/tag.go
+++ chapter_04/notify/app/server/internal/biz/tag.go
@@ -11,6 +11,23 @@ type Tag struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+type TemplateTagRelation struct {
+	Id           int
+	TemplateUuid string
+	TagUuid      string
+	Status       int8
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
+
+type UserTagRelation struct {
+	Id        int
+	UserUuid  string
+	TagUuid   string
+	Status    int8
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
 
 type TagRepo interface {
 }
