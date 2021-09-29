42de66d201f679b8fef44afb296b730200909d62
张斌 <zhangbin@maizijf.com>
2021-09-30 00:12:33 +0800

    添加中间表的schema

--- chapter_04/notify/app/server/internal/data/ent/schema/templatetagrelation.go
+++ chapter_04/notify/app/server/internal/data/ent/schema/templatetagrelation.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/field"
+	"time"
+)
 
 // TemplateTagRelation holds the schema definition for the TemplateTagRelation entity.
 type TemplateTagRelation struct {
@@ -9,7 +14,19 @@ type TemplateTagRelation struct {
 
 // Fields of the TemplateTagRelation.
 func (TemplateTagRelation) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Time("created_at").SchemaType(map[string]string{
+			dialect.MySQL: "datetime",
+		}).Default(time.Now).Immutable(),
+		field.Time("updated_at").SchemaType(map[string]string{
+			dialect.MySQL: "datetime",
+		}).Default(time.Now).
+			UpdateDefault(time.Now),
+		field.String("template_uuid").Nillable().NotEmpty(),
+		field.String("tag_uuid").Nillable().NotEmpty(),
+		field.Int("status").
+			Default(1).NonNegative(),
+	}
 }
 
 // Edges of the TemplateTagRelation.
--- chapter_04/notify/app/server/internal/data/ent/schema/usertagrelation.go
+++ chapter_04/notify/app/server/internal/data/ent/schema/usertagrelation.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/field"
+	"time"
+)
 
 // UserTagRelation holds the schema definition for the UserTagRelation entity.
 type UserTagRelation struct {
@@ -9,7 +14,19 @@ type UserTagRelation struct {
 
 // Fields of the UserTagRelation.
 func (UserTagRelation) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Time("created_at").SchemaType(map[string]string{
+			dialect.MySQL: "datetime",
+		}).Default(time.Now).Immutable(),
+		field.Time("updated_at").SchemaType(map[string]string{
+			dialect.MySQL: "datetime",
+		}).Default(time.Now).
+			UpdateDefault(time.Now),
+		field.String("user_uuid").Nillable().NotEmpty(),
+		field.String("tag_uuid").Nillable().NotEmpty(),
+		field.Int("status").
+			Default(1).NonNegative(),
+	}
 }
 
 // Edges of the UserTagRelation.
