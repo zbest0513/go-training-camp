8a736f797c23c6e137ce457a334669783cb9065f
zbest <705359715@qq.com>
2021-09-30 19:25:51 +0800

    提交模版相关的biz功能实现 1.创建模版 2.启用/禁用模版 3.删除模版 4.根据模版查询用户列表 5.根据模版查询标签列表

--- chapter_04/notify/app/server/internal/biz/template.go
+++ chapter_04/notify/app/server/internal/biz/template.go
@@ -2,6 +2,11 @@ package biz
 
 import (
 	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"log"
+	"notify-server/internal/pkg/enum"
+	"sync"
 	"time"
 )
 
@@ -21,4 +26,104 @@ type TemplateRepo interface {
 	UpdateTemplate(context.Context, Template) (int, error)
 	UpdateStatus(context.Context, string, int) (int, error)
 	DeleteTemplate(context.Context, string) (int, error)
+	DisbandTags(ctx context.Context, uuid string) (int, error)
+	UpdateTagRelationsStatus(ctx context.Context, uuid string, status int) (int, error)
+	QueryTags(ctx context.Context, uuid string) ([]*Tag, error)
+}
+
+type TemplateUseCase struct {
+	repo    TemplateRepo
+	tagRepo TagRepo
+}
+
+func NewTemplateUseCase(repo TemplateRepo, tagRepo TagRepo) *TemplateUseCase {
+	return &TemplateUseCase{
+		repo:    repo,
+		tagRepo: tagRepo,
+	}
+}
+
+func (tu *TemplateUseCase) CreateTemplate(ctx context.Context, template Template) (*Template, error) {
+	createTemplate, err := tu.repo.CreateTemplate(ctx, template)
+	if err != nil {
+		return nil, errors.WithMessage(err, "创建模版失败")
+	}
+	return createTemplate, nil
+}
+
+func (tu *TemplateUseCase) UpdateTemplate(ctx context.Context, template Template) error {
+	_, err := tu.repo.UpdateTemplate(ctx, template)
+	if err != nil {
+		return errors.WithMessage(err, fmt.Sprintf("修改模版[%v]失败", template))
+	}
+	return nil
+}
+
+func (tu *TemplateUseCase) DeleteTemplate(ctx context.Context, uuid string) error {
+	count, err := tu.repo.DeleteTemplate(ctx, uuid)
+
+	if err != nil {
+		return errors.WithMessage(err, fmt.Sprintf("删除模版[%v]失败", uuid))
+	}
+	if count > 0 {
+		count, err = tu.repo.DisbandTags(ctx, uuid)
+		if err != nil {
+			return errors.WithMessage(err, fmt.Sprintf("删除标签时,解除tag关系失败"))
+		}
+	}
+	log.Println(fmt.Sprintf("删除模版[%v]成功", uuid))
+	return nil
+}
+
+func (tu *TemplateUseCase) UpdateTemplateStatus(ctx context.Context, uuid string, status int) error {
+	count, err := tu.repo.UpdateStatus(ctx, uuid, status)
+	if err != nil {
+		return errors.WithMessage(err, fmt.Sprintf("更改模版[%v]状态[%v]", uuid, status))
+	}
+	if count > 0 {
+		var tStatus int
+		if status == enum.TEMPLATE_STATUS_AVAILABLE { //启用
+			tStatus = enum.RELATION_TEMPLATE_TAG_AVAILABLE
+		} else { //禁用
+			tStatus = enum.RELATION_TEMPLATE_TAG_UNAVAILABLE
+		}
+		count, err = tu.repo.UpdateTagRelationsStatus(ctx, uuid, tStatus)
+		if err != nil {
+			return errors.WithMessage(err, fmt.Sprintf("解除模版[%v]的标签关系失败", uuid))
+		}
+	}
+	return nil
+}
+
+func (tu *TemplateUseCase) QueryUsers(ctx context.Context, uuid string) ([]*User, error) {
+	tags, err := tu.QueryTags(ctx, uuid)
+	if err != nil {
+		return nil, errors.WithMessage(err, fmt.Sprintf("根据模版[%v]查询用户列表方法查询标签失败", uuid))
+	}
+	result := make([]*User, 0)
+	m := make(map[string][]*User)
+	//TODO 换掉wait group
+	var wg sync.WaitGroup
+	for _, tag := range tags {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			users, err := tu.tagRepo.QueryUsers(ctx, tag.Uuid)
+			if err != nil {
+			}
+			m[tag.Name] = users
+		}()
+	}
+	for _, mk := range m {
+		result = append(result, mk...)
+	}
+	return result, nil
+}
+
+func (tu *TemplateUseCase) QueryTags(ctx context.Context, uuid string) ([]*Tag, error) {
+	tags, err := tu.repo.QueryTags(ctx, uuid)
+	if err != nil {
+		return nil, errors.WithMessage(err, fmt.Sprintf("根据模版[%v]查询标签失败", uuid))
+	}
+	return tags, nil
 }
--- chapter_04/notify/app/server/internal/data/template_repo.go
+++ chapter_04/notify/app/server/internal/data/template_repo.go
@@ -7,7 +7,9 @@ import (
 	"github.com/pkg/errors"
 	"notify-server/internal/biz"
 	"notify-server/internal/data/ent"
+	tgWhere "notify-server/internal/data/ent/tag"
 	tWhere "notify-server/internal/data/ent/template"
+	ttWhere "notify-server/internal/data/ent/templatetagrelation"
 	"time"
 )
 
@@ -45,8 +47,7 @@ func (tr *templateRepo) CreateTemplate(ctx context.Context, template biz.Templat
 
 func (tr *templateRepo) UpdateTemplate(ctx context.Context, template biz.Template) (int, error) {
 	count, err := tr.data.Template.Update().Where(tWhere.UUIDEQ(template.Uuid)).
-		SetDesc(template.Desc).SetContent(template.Content).
-		SetStatus((int)(template.Status)).SetUpdatedAt(time.Now()).Save(ctx)
+		SetDesc(template.Desc).SetContent(template.Content).SetUpdatedAt(time.Now()).Save(ctx)
 	if err != nil {
 		return 0, errors.WithMessage(err, "repo更新Tag数据失败")
 	}
@@ -68,4 +69,49 @@ func (tr *templateRepo) DeleteTemplate(ctx context.Context, uuid string) (int, e
 		return 0, errors.WithMessage(err, "repo删除tag失败")
 	}
 	return count, nil
+}
+
+func (tr *templateRepo) DisbandTags(ctx context.Context, uuid string) (int, error) {
+	count, err := tr.data.TemplateTagRelation.Delete().Where(ttWhere.TemplateUUIDEQ(uuid)).Exec(ctx)
+	if err != nil {
+		return count, errors.WithMessage(err, fmt.Sprintf("删除template[%v]", uuid))
+	}
+	return count, nil
+}
+
+func (tr *templateRepo) UpdateTagRelationsStatus(ctx context.Context, uuid string, status int) (int, error) {
+	count, err := tr.data.TemplateTagRelation.Update().SetStatus(status).Where(ttWhere.TemplateUUIDEQ(uuid)).Save(ctx)
+	if err != nil {
+		return count, errors.WithMessage(err, fmt.Sprintf("修改template[%v]的tag关系失败[%v]", uuid, status))
+	}
+	return count, nil
+}
+
+func (tr *templateRepo) QueryTags(ctx context.Context, uuid string) ([]*biz.Tag, error) {
+	all, err := tr.data.TemplateTagRelation.Query().Where(ttWhere.TemplateUUIDEQ(uuid)).All(ctx)
+	if err != nil {
+		return nil, errors.WithMessage(err, fmt.Sprintf("查询模版[%v]关联的tags失败", uuid))
+	}
+	ids := make([]int, len(all))
+	for i, relation := range all {
+		ids[i] = relation.ID
+	}
+	tags, err := tr.data.Tag.Query().Where(tgWhere.IDIn(ids...)).All(ctx)
+
+	if err != nil {
+		return nil, errors.WithMessage(err, fmt.Sprintf("查询标签[%v]失败", ids))
+	}
+	result := make([]*biz.Tag, len(tags))
+	for i, tag := range tags {
+		result[i] = &biz.Tag{
+			tag.ID,
+			tag.UUID,
+			tag.Name,
+			tag.Desc,
+			(int8)(tag.Status),
+			tag.CreatedAt,
+			tag.UpdatedAt,
+		}
+	}
+	return result, nil
 }
